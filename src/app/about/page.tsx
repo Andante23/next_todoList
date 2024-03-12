16bca7b3ed7afc50298faa159f496254823cf54c
andan <andante1104@naver.com>
2024-03-12 12:20:23 +0900

    Feat: 회사정보를 불러와서 회사소개하는 about폴더의 page 페이지  작성 완료

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,40 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+// import { CompanInfo } from "../types/todo";
+import React from "react";
+
 function AboutPage() {
-  return <></>;
+  // 회사데이터를 서버로 부터 가져오는 로직 작성
+  const {
+    data: companyInfo,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:4000/companinfo`);
+      const companinfo = await res.json();
+      return companinfo;
+    },
+  });
+
+  if (isLoading) {
+    return <div>로딩중이에요</div>;
+  }
+
+  if (isError) {
+    return <div>에러에요</div>;
+  }
+
+  return (
+    <>
+      <div>
+        <h1>{companyInfo.name}</h1>
+        <p>{companyInfo.description}</p>
+      </div>
+    </>
+  );
 }
 
 export default AboutPage;
---
+++ src/app/types/todo.ts
@@ -0,0 +1,11 @@
+export type Todos = {
+    id:string,
+    title:string,
+    content:string,
+    isDone:boolean
+}
+
+export type CompanInfo ={
+    name: string,
+    description:string
+}
